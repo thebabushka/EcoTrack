435d54b47b017bd740c948e6d95077745021f9ae
aadya garg <aadya@aadyas-MacBook-Air.local>
2025-08-19 16:34:10 -0700

    Initial Commit

---
+++ EcoTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoTrack
+//
+//  Created by aadya garg on 8/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EcoTrackTests/EcoTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcoTrackTests.swift
+//  EcoTrackTests
+//
+//  Created by aadya garg on 8/19/25.
+//
+
+import Testing
+@testable import EcoTrack
+
+struct EcoTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
